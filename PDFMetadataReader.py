a56a0b8f8188e5b0130291413bd76cde2f7062b8
octave-FR <octave.main@gmail.com>
2015-11-10 15:40:28 +0100

    ajout de commentaire

--- PDFMetadataReader.py
+++ PDFMetadataReader.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: iso-8859-15 -*-
+
 import sys, re, datetime
 	
 def usage() :
@@ -46,8 +47,10 @@ groups = re.search(r'/Info \d{1,3} \d{1} R', data)
 
 #split par "endobj" et recherche de la zone avec nos infos
 for raw_meta in data.split("endobj") :
+	#on enleve le "/Info " et on remplace le 'R' par 'obj' pour effectuer une recherche dans le fichier
 	if groups.group(0).split(" ", 1)[1].replace('R', 'obj') in raw_meta :
-		buf_meta = raw_meta.split("<<")
+		#on split pour n'avoir que les metadonnées
+		buf_meta = raw_meta.split("<<")[1]
 		
 #si la variable 'buf_meta' n'existe pas, elle n'est pas initialisé, donc on a pas trouvé les meta. 
 if 'buf_meta' not in locals():
@@ -58,8 +61,8 @@ tab_values_raw = []
 
 buf = ""
 first = True
-# on repere chaque champs 
-for x in re.split('(/[A-Z])', buf_meta[1]) :
+# on repere chaque champs (ils commencent toujours par un '/' et une lettre majuscule)
+for x in re.split('(/[A-Z])', buf_meta) :
 	if x != '' :
 		if x[0] in "/" :
 			# pour ne as déclancher a la premiere occurence
@@ -76,16 +79,16 @@ for meta in tab_values_raw :
 	#si on a un '<', nous devons lire une hexstring
 	if '<' in meta : 
 		name = meta.split('<')[0]
-		value_raw = meta.split('<')[1][:-1]
-
+		value_raw = meta.split('<')[1].replace('>', '')
+		
 		if value_raw[:4] == "FEFF" :
-			#on doit prendre 2 caractere sur 2 XX..XX..XX..XX
+			#on doit prendre 2 caractere sur 2 XX..XX..XX..XX et on ne prend pas les 3 premieres cases qui represente le FE,FF,00
 			buf_value = ['0x'+value_raw[meta:meta+2] for meta in range(0, len(value_raw), 2)][3:]
 		
 			#on transforme en int puis on join la liste pour avoir une chaine de caractere
 			value = "".join([chr(int(buf_value[y], 16)) for y in range(0, len(buf_value), 2)])
 
-		print name + " : " + value
+			print name + " : " + value
 	else :
 
 		#si on trouve la string "D:", nous avons une information de date
@@ -94,6 +97,7 @@ for meta in tab_values_raw :
 
 			print meta.split("D:")[0].replace("(", '') + " : " + dateString
 		else :
-			line = " ".join(meta.replace("\\", '').split("(")).replace(")", '').split(" ")
+			#sinon les données sont en clair 
+			line = " ".join(meta.replace("\\", '').split("(")).replace(")", '').split(" ") #permet de netoyer la chaine de caractere
 			print line[0] + " : " + " ".join(line[1:])
 
